1b851732e3cfa801ccaa63e5c93bca73f605c343
Rahul Salvi <rsalvi006@me.com>
2019-04-26 16:37:18 -0700

    reorganizing

---
+++ examples/aemnet_test.cpp
@@ -0,0 +1,11 @@
+#include "WProgram.h"
+#include "aemnet_utils.h"
+
+void setup() {}
+
+void loop() {}
+
+extern "C" int main(void) {
+    setup();
+    while (1) { loop(); }
+}
---
+++ src/aemnet_utils.h
@@ -0,0 +1,23 @@
+#ifndef AEMNET_UTILS_H
+#define AEMNET_UTILS_H
+
+#include <cstdint>
+
+namespace aemnet_utils {
+
+    typedef uint32_t fixed_point_t; // 16.16 format
+
+    void begin();
+    void update();
+
+    fixed_point_t rpm();
+    fixed_point_t thr();
+    fixed_point_t iat();
+    fixed_point_t clt();
+    fixed_point_t afr();
+    fixed_point_t bat();
+    fixed_point_t fpr();
+
+} // namespace aemnet_utils
+
+#endif // AEMNET_UTILS_H
