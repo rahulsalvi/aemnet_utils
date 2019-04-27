ca7f69c0dd5a58be27ff4e83052202f2a5c99e0b
Rahul Salvi <rsalvi006@me.com>
2019-04-26 17:09:52 -0700

    filled out example program

--- examples/aemnet_test.cpp
+++ examples/aemnet_test.cpp
@@ -1,9 +1,41 @@
 #include "WProgram.h"
 #include "aemnet_utils.h"
 
-void setup() {}
+using aemnet_utils::fixed_point_t;
 
-void loop() {}
+inline float fixed_to_float(fixed_point_t a);
+
+void setup() {
+    Serial.begin(115200);
+    aemnet_utils::begin();
+}
+
+void loop() {
+    aemnet_utils::update();
+
+    Serial.println("------------------------------");
+    Serial.print("RPM: ");
+    Serial.println(fixed_to_float(aemnet_utils::rpm()));
+    Serial.print("THR: ");
+    Serial.println(fixed_to_float(aemnet_utils::thr()));
+    Serial.print("IAT: ");
+    Serial.println(fixed_to_float(aemnet_utils::iat()));
+    Serial.print("CLT: ");
+    Serial.println(fixed_to_float(aemnet_utils::clt()));
+    Serial.print("AFR: ");
+    Serial.println(fixed_to_float(aemnet_utils::afr()));
+    Serial.print("BAT: ");
+    Serial.println(fixed_to_float(aemnet_utils::bat()));
+    Serial.print("FPR: ");
+    Serial.println(fixed_to_float(aemnet_utils::fpr()));
+    Serial.println("------------------------------");
+
+    delay(100);
+}
+
+inline float fixed_to_float(fixed_point_t a) {
+    return ((float)a / (float)(1 << 16));
+}
 
 extern "C" int main(void) {
     setup();
